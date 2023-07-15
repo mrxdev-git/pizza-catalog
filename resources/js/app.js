1d8502688024c04f283bf9f977bd7d1938a90cbd
Mr X <nertic.mihai@gmail.com>
2023-07-15 20:42:40 +0300

    App.js added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,31 @@
-import './bootstrap';
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import axios from 'axios';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+import App from './components/App.vue';
+
+import Ingredients from './components/Ingredients.vue';
+import Pizzas from './components/Pizzas.vue';
+
+const routes = [
+    { path: '/', redirect: '/ingredients' },
+    { path: '/ingredients', component: Ingredients },
+    { path: '/pizzas', component: Pizzas }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+const app = createApp(App);
+
+app.use(router);
+
+app.config.globalProperties.$http = axios;
+
+app.mount('#app');
